27ef483887323da59cfe935d57b484ad65839223
ilyas-draissia <ilyesdraissia@gmail.com>
2026-06-24 07:39:12 +0100

    few changes and bugs eliminated
    -changed the mechanism that changes the background color once scrolling for few lines which enhances performance .
    -updated the posters with alt text for better accessibility .

--- script.js
+++ script.js
@@ -1,117 +1,19 @@
 const issues = document.querySelectorAll('.issue');
 const footer = document.querySelector('.footer');
+const colors =["#F6E0A4","#FF608C","#FFFFFF","#2FCCC3","#DE712D","#FFBE00","#1D3FBB","#E30512"];
 
-let issuesheights =[];
-issues.forEach(issue =>{
-    const height = issue.scrollHeight;
-    issuesheights.push(height);
-});
-// function calculate(num1,num2,per){
-//     result = Math.sumPrecise(issuesheights.slice(num1,num2));
-//     if (per != 0 ){
-//         const d=((result * per) / 100);
-//         return result - d;
-//     }
-//     else{
-//         return result;
-//     }
-// }
-// const scrollFunction = () => {
-//  if (window.scrollY < calculate(0,1,10)){
-//     issues.forEach(issue=>{
-//         issue.style.backgroundColor = "#F6E0A4";
-//         footer.style.backgroundColor = "#F6E0A4";
-//     });
-// }if (window.scrollY > calculate(0,1) && window.scrollY < calculate(0,2))                {    
-//     issues.forEach(issue=>{
-//         issue.style.backgroundColor = "#FF608C";
-//         footer.style.backgroundColor = "#FF608C";
-//     });
-// }if (window.scrollY > calculate(0,2) && window.scrollY < calculate(0,3)){
-//     issues.forEach(issue=>{
-//         issue.style.backgroundColor = "#FFFFFF";
-//         footer.style.backgroundColor = "#FFFFFF";
-//     });
-// }if (window.scrollY > calculate(0,3) && window.scrollY < calculate(0,4)){    
-//     issues.forEach(issue=>{
-//         issue.style.backgroundColor = "#2FCCC3";
-//         footer.style.backgroundColor = "#2FCCC3";
-//     });
-// }if (window.scrollY > calculate(0,4) && window.scrollY < calculate(0,5)){
-//     issues.forEach(issue=>{
-//         issue.style.backgroundColor = "#DE712D";
-//         footer.style.backgroundColor = "#DE712D";
-//     });
-// }if (window.scrollY > calculate(0,5) && window.scrollY < calculate(0,6)){
-//     issues.forEach(issue=>{
-//         issue.style.backgroundColor = "#FFBE00";
-//         footer.style.backgroundColor = "#FFBE00";
-//     });
-// }if (window.scrollY > calculate(0,6) && window.scrollY < calculate(0,7)){    
-//     issues.forEach(issue=>{
-//         issue.style.backgroundColor = "#1D3FBB";
-//         footer.style.backgroundColor = "#1D3FBB";
-//     });
-// }if (window.scrollY > calculate(0,7,0)){   
-//     issues.forEach(issue=>{
-//         issue.style.backgroundColor = "#E30512";
-//         footer.style.backgroundColor = "#E30512";
-//     });
-// }
-// else{};
-// }
-function calculate(num1,num2){
-    result = Math.sumPrecise(issuesheights.slice(num1,num2));
-    return result;
-}
-const scrollFunction = () => {
- if (window.scrollY < calculate(0,1)){
-    issues.forEach(issue=>{
-        issue.style.backgroundColor = "#F6E0A4";
-        footer.style.backgroundColor = "#F6E0A4";
-    });
-}if (window.scrollY > calculate(0,1) && window.scrollY < calculate(0,2))                {    
-    issues.forEach(issue=>{
-        issue.style.backgroundColor = "#FF608C";
-        footer.style.backgroundColor = "#FF608C";
-    });
-}if (window.scrollY > calculate(0,2) && window.scrollY < calculate(0,3)){
-    issues.forEach(issue=>{
-        issue.style.backgroundColor = "#FFFFFF";
-        footer.style.backgroundColor = "#FFFFFF";
-    });
-}if (window.scrollY > calculate(0,3) && window.scrollY < calculate(0,4)){    
-    issues.forEach(issue=>{
-        issue.style.backgroundColor = "#2FCCC3";
-        footer.style.backgroundColor = "#2FCCC3";
-    });
-}if (window.scrollY > calculate(0,4) && window.scrollY < calculate(0,5)){
-    issues.forEach(issue=>{
-        issue.style.backgroundColor = "#DE712D";
-        footer.style.backgroundColor = "#DE712D";
-    });
-}if (window.scrollY > calculate(0,5) && window.scrollY < calculate(0,6)){
-    issues.forEach(issue=>{
-        issue.style.backgroundColor = "#FFBE00";
-        footer.style.backgroundColor = "#FFBE00";
-    });
-}if (window.scrollY > calculate(0,6) && window.scrollY < calculate(0,7)){    
-    issues.forEach(issue=>{
-        issue.style.backgroundColor = "#1D3FBB";
-        footer.style.backgroundColor = "#1D3FBB";
-    });
-}if (window.scrollY > calculate(0,7)){   
-    issues.forEach(issue=>{
-        issue.style.backgroundColor = "#E30512";
-        footer.style.backgroundColor = "#E30512";
-    });
-}
-else{};
-}
-scrollFunction();
-window.addEventListener('scroll',()=>{
-    scrollFunction();
-});
+const observer = new IntersectionObserver(((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            const index = [...issues].indexOf(entry.target);
+            const color = colors[index];
+            issues.forEach((issue)=>{issue.style.backgroundColor = color});
+            footer.style.backgroundColor = color;
+        }});
+}),{threshold : 0.6});
+issues.forEach((issue)=>{
+    observer.observe(issue);
+})
 
 
 
